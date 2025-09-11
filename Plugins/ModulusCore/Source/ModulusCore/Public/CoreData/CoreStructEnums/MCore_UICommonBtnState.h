cfaa2553b8ed9feedec051f181005b4f359fe73e
midnight-pxl <midnightpixelstudio@gmail.com>
2025-09-10 21:14:46 -0500

    Initial input actions

---
+++ Plugins/ModulusCore/Source/ModulusCore/Public/CoreData/CoreStructEnums/MCore_UICommonBtnState.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2025, Midnight Pixel Studio LLC. All Rights Reserved
+
+#pragma once
+
+#include "MCore_UICommonBtnState.generated.h"
+
+UENUM(BlueprintType)
+enum class EMCore_ButtonState : uint8
+{
+    Normal,
+    Hovered,
+    Pressed,
+    SelectedNormal,
+    SelectedHovered,
+    Disabled
+};
