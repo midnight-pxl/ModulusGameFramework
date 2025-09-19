2390e0a27ec74e2cd9e3f3b543377bb5fb417769
David White <dbwhite@mccarthy.com>
2025-09-19 10:19:28 -0500

    init PDA for standardized input action metadata

---
+++ Plugins/ModulusCore/Source/ModulusCore/Private/CoreData/CoreDataAssets/InputActionsData/MCore_PDA_InputActionMetadata.cpp
@@ -0,0 +1,28 @@
+﻿// Copyright 2025, Midnight Pixel Studio LLC. All Rights Reserved
+
+
+#include "CoreData/CoreDataAssets/InputActionsData/MCore_PDA_InputActionMetadata.h"
+
+UMCore_PDA_InputActionMetadata::UMCore_PDA_InputActionMetadata()
+{
+}
+
+FPrimaryAssetId UMCore_PDA_InputActionMetadata::GetPrimaryAssetId() const
+{
+	return Super::GetPrimaryAssetId();
+}
+
+FString UMCore_PDA_InputActionMetadata::GetPrimaryAssetName() const
+{
+	return FString(TEXT(""));
+}
+
+bool UMCore_PDA_InputActionMetadata::IsKeyAllowed(FKey Key) const
+{
+	return true;
+}
+
+bool UMCore_PDA_InputActionMetadata::IsPlatformSupported(const FString& PlatformName) const
+{
+	return false;
+}
