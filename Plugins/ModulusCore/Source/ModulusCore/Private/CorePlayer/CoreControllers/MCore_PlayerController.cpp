fa7c579d8ea5a5da8fbf27d639af7fd4d19f8034
midnight-pxl <midnightpixelstudio@gmail.com>
2025-09-22 20:14:43 -0500

    init MCore_PlayerController

---
+++ Plugins/ModulusCore/Source/ModulusCore/Private/CorePlayer/CoreControllers/MCore_PlayerController.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright 2025, Midnight Pixel Studio LLC. All Rights Reserved
+
+
+#include "CorePlayer/CoreControllers/MCore_PlayerController.h"
+
+#include "Camera/CameraActor.h"
+#include "Kismet/GameplayStatics.h"
+
+void AMCore_PlayerController::OnPossess(APawn* aPawn)
+{
+	Super::OnPossess(aPawn);
+
+	TArray<AActor*> FoundCameras;
+	UGameplayStatics::GetAllActorsOfClassWithTag(this, ACameraActor::StaticClass(), FName("MCore.MainMenu.Default"),FoundCameras);
+
+	if (!FoundCameras.IsEmpty())
+	{
+		SetViewTargetWithBlend(FoundCameras[0]);
+	}
+}
---
+++ Plugins/ModulusCore/Source/ModulusCore/Public/CorePlayer/CoreControllers/MCore_PlayerController.h
@@ -0,0 +1,19 @@
+﻿// Copyright 2025, Midnight Pixel Studio LLC. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "MCore_PlayerController.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MODULUSCORE_API AMCore_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+
+protected:
+	virtual void OnPossess(APawn* aPawn) override;
+};
