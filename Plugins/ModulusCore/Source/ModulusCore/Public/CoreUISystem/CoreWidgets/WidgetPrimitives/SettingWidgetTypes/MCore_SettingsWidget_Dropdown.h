9a850be8f4b7124d600de85e74c46c5e626eb7ed
David White <dbwhite@mccarthy.com>
2025-10-27 13:15:33 -0500

    init slider and dropdown setting widget cpp classes

---
+++ Plugins/ModulusCore/Source/ModulusCore/Public/CoreUISystem/CoreWidgets/WidgetPrimitives/SettingWidgetTypes/MCore_SettingsWidget_Dropdown.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2025, Midnight Pixel Studio LLC. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MCore_SettingsWidget_Base.h"
+#include "MCore_SettingsWidget_Dropdown.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MODULUSCORE_API UMCore_SettingsWidget_Dropdown : public UMCore_SettingsWidget_Base
+{
+	GENERATED_BODY()
+};
---
+++ Plugins/ModulusCore/Source/ModulusCore/Public/CoreUISystem/CoreWidgets/WidgetPrimitives/SettingWidgetTypes/MCore_SettingsWidget_Slider.h
@@ -0,0 +1,16 @@
+﻿// Copyright 2025, Midnight Pixel Studio LLC. All Rights Reserved
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "MCore_SettingsWidget_Base.h"
+#include "MCore_SettingsWidget_Slider.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class MODULUSCORE_API UMCore_SettingsWidget_Slider : public UMCore_SettingsWidget_Base
+{
+	GENERATED_BODY()
+};
