343c4b8f34a60061c12ed8de797a0cc924eea3d5
midnight-pxl <midnightpixelstudio@gmail.com>
2026-02-01 15:14:04 -0600

    finalizing workflow of widget ownership between PlayerController and UISubsystem

--- Plugins/ModulusCore/Source/ModulusCore/Private/CorePlayer/MCore_PlayerController.cpp
+++ Plugins/ModulusCore/Source/ModulusCore/Private/CorePlayer/MCore_PlayerController.cpp
@@ -1,21 +1,62 @@
 ﻿// Copyright 2025, Midnight Pixel Studio LLC. All Rights Reserved
 
-
 #include "CorePlayer/MCore_PlayerController.h"
+
+#include "EnhancedInputSubsystems.h"
 #include "CoreUI/Widgets/MCore_PrimaryGameLayout.h"
 #include "CoreData/Tags/MCore_SettingsTags.h"
+#include "CoreData/Tags/MCore_UILayerTags.h"
 #include "CoreData\Logging\LogModulusUI.h"
+#include "CoreUI/MCore_UISubsystem.h"
 #include "CoreUI/Widgets/Primitives/MCore_ActivatableBase.h"
-#include "Net/UnrealNetwork.h"
 
-AMCore_PlayerController::AMCore_PlayerController(const FObjectInitializer& ObjectInitializer)
-	: Super(ObjectInitializer)
+AMCore_PlayerController::AMCore_PlayerController()
 {
+	PrimaryWidgetLayer = MCore_UILayerTags::UI_Layer_Game;
 }
 
 void AMCore_PlayerController::BeginPlay()
 {
 	Super::BeginPlay();
+	
+	if (IsLocalController()) { return; }
+	
+	InitializeUISystem();
+}
+
+void AMCore_PlayerController::EndPlay(const EEndPlayReason::Type EndPlayReason)
+{
+	/** Perform cleanup */
+	if (IsValid(PrimaryWidget))
+	{
+		PrimaryWidget->DeactivateWidget();
+		PrimaryWidget = nullptr;
+	}
+	
+	/** Unbind delegate(s) */
+	if (ULocalPlayer* LocalPlayer = GetLocalPlayer())
+	{
+		if (UMCore_UISubsystem* UISubsystem = LocalPlayer->GetSubsystem<UMCore_UISubsystem>())
+		{
+			UISubsystem->OnPrimaryGameLayoutReady.RemoveDynamic(this, &ThisClass::OnPrimaryGameLayoutReady);
+		}
+	}
+	
+	bUISystemInitialized = false;	
+	Super::EndPlay(EndPlayReason);
+}
+
+void AMCore_PlayerController::OnPossess(APawn* aPawn)
+{
+	Super::OnPossess(aPawn);
+	
+	ULocalPlayer* LocalPlayer = GetLocalPlayer();
+	if (!IsLocalController() || !LocalPlayer) { return; }
+	
+	/**
+	 * Subclasses override this function to customize
+	 * OnPossess logic
+	 */
 }
 
 void AMCore_PlayerController::SetupInputComponent()
@@ -23,14 +64,62 @@ void AMCore_PlayerController::SetupInputComponent()
 	Super::SetupInputComponent();
 }
 
-void AMCore_PlayerController::OnPossess(APawn* aPawn)
+void AMCore_PlayerController::InitializeUISystem()
 {
-	Super::OnPossess(aPawn);
+	ULocalPlayer* LocalPlayer = GetLocalPlayer();
+	if (!LocalPlayer) { return; }
+	
+	UMCore_UISubsystem* UISubsystem = LocalPlayer->GetSubsystem<UMCore_UISubsystem>();
+	if (!UISubsystem) { return; }
+	
+	if (UISubsystem->HasPrimaryGameLayout())
+	{
+		OnUISystemReady(UISubsystem);
+	}
+	else
+	{
+		UISubsystem->OnPrimaryGameLayoutReady.AddDynamic(this, &ThisClass::OnPrimaryGameLayoutReady);
+		UE_LOG(LogModulusUI, Verbose, TEXT("PlayerController: Waiting for PrimaryGameLayout..."));
+	}
 }
 
-void AMCore_PlayerController::ToggleInGameMenu()
+void AMCore_PlayerController::OnPrimaryGameLayoutReady(UMCore_PrimaryGameLayout* Layout)
 {
+	if (bUISystemInitialized) { return; }
+	
+	ULocalPlayer* LocalPlayer = GetLocalPlayer();
+	if (!LocalPlayer) { return; }
+	
+	UMCore_UISubsystem* UISubsystem = LocalPlayer->GetSubsystem<UMCore_UISubsystem>();
+	if (!UISubsystem) { return; }
+	
+	UISubsystem->OnPrimaryGameLayoutReady.RemoveDynamic(this, &ThisClass::OnPrimaryGameLayoutReady);
+	OnUISystemReady(UISubsystem);
 }
+
+void AMCore_PlayerController::OnUISystemReady_Implementation(UMCore_UISubsystem* UISubsystem)
+{
+	if (bUISystemInitialized) { return; }
+	
+	if (PrimaryWidgetClass && PrimaryWidgetLayer.IsValid())
+	{
+		PrimaryWidget = UISubsystem->PushWidgetToLayer(PrimaryWidgetClass, PrimaryWidgetLayer);
+		
+		if (PrimaryWidget)
+		{
+			UE_LOG(LogModulusUI, Log, TEXT("PlayerController::OnUISystemReady: Pushed '%s' to layer '%s'"),
+				*PrimaryWidgetClass->GetName(), *PrimaryWidgetLayer.ToString());
+		}
+		else
+		{
+			UE_LOG(LogModulusUI, Error, TEXT("PlayerController: Failed to push '%s' to layer '%s'"),
+				*PrimaryWidgetClass->GetName(), *PrimaryWidgetLayer.ToString());
+		}
+	}
+	
+	bUISystemInitialized = true;
+}
+
 
 
 
--- Plugins/ModulusCore/Source/ModulusCore/Public/CorePlayer/MCore_PlayerController.h
+++ Plugins/ModulusCore/Source/ModulusCore/Public/CorePlayer/MCore_PlayerController.h
@@ -3,15 +3,28 @@
 #pragma once
 
 #include "CoreMinimal.h"
+#include "GameplayTagContainer.h"
 
 #include "GameFramework/PlayerController.h"
 #include "MCore_PlayerController.generated.h"
 
 class UInputAction;
+class UMCore_UISubsystem;
 class UInputMappingContext;
+class UCommonActivatableWidget;
 
 /**
- * Dedicated Main Menu PlayerController
+ * Base PlayerController for ModulusCore
+ *
+ * Handles:
+ * - HUD widget creation and push to GameLayer
+ * - Deferred UI setup when PrimaryGameLayout not immediately ready
+ * - Input context management (DefaultModulusContext)
+ *
+ * Usage:
+ * 1. Create Blueprint subclass
+ * 2. Set HUDWidgetClass to your HUD widget
+ * 3. Set as default PlayerController in GameMode
  */
 UCLASS()
 class MODULUSCORE_API AMCore_PlayerController : public APlayerController
@@ -19,7 +32,7 @@ class MODULUSCORE_API AMCore_PlayerController : public APlayerController
 	GENERATED_BODY()
 	
 public:
-	AMCore_PlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+	AMCore_PlayerController();
 	
 	//~ Begin AActor Interface
 	virtual void BeginPlay() override;
@@ -28,18 +41,45 @@ public:
 protected:
 	//~ Begin APlayerController Interface
 	virtual void SetupInputComponent() override;
-	virtual void OnPossess(APawn* aPawn) override;	
+	virtual void EndPlay(const EEndPlayReason::Type EndPlayReason) override;
+	virtual void OnPossess(APawn* aPawn) override;
 	//~ End APlayerController Interface
+
+	/**
+	 * Widget to push when UI system ready
+	 * HUD for gameplay level(s), MainMenu for MainMenu level, etc.) 
+	 */
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI")
+	TSubclassOf<UCommonActivatableWidget> PrimaryWidgetClass;
 	
 	/**
-	 * Input Actions
+	 * Layer to push PrimaryWidgetClass onto.
+	 * Default: UI_Layer_Game
 	 */
-	
-	void ToggleInGameMenu();
-	
-	UPROPERTY(EditDefaultsOnly, Category = "Modulus|Input")
-	TObjectPtr<UInputMappingContext> DefaultModulusContext;
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, Category = "UI", meta = (Categories = "MCore.UI.Layer"))
+	FGameplayTag PrimaryWidgetLayer;
 
 	UPROPERTY(EditDefaultsOnly, Category = "Modulus|Input")
 	TArray<TObjectPtr<UInputMappingContext>> InputContexts;
+	
+	/**
+ * Called when UISubsystem and PrimaryGameLayout are ready.
+ * Default pushes PrimaryWidgetClass to PrimaryWidgetLayer.
+ * Override for custom setup (clear layers, push additional widgets, etc.)
+ */
+	UFUNCTION(BlueprintNativeEvent, Category = "UI")
+	void OnUISystemReady(UMCore_UISubsystem* UISubsystem);
+	virtual void OnUISystemReady_Implementation(UMCore_UISubsystem* UISubsystem);
+
+	/** Widget this controller created. Cleaned up in EndPlay. */
+	UPROPERTY(Transient, BlueprintReadOnly, Category = "UI")
+	TObjectPtr<UCommonActivatableWidget> PrimaryWidget;
+	
+private:
+	UFUNCTION()
+	void OnPrimaryGameLayoutReady(UMCore_PrimaryGameLayout* Layout);
+	
+	void InitializeUISystem();
+	
+	bool bUISystemInitialized{false};
 };
