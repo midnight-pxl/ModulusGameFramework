f0cd704be037d819baa4938ff94d185e9e2552f9
midnight-pxl <midnightpixelstudio@gmail.com>
2026-03-24 22:41:50 -0500

    Created new GameplayTags file for default menu tab tags listings

---
+++ Plugins/ModulusCore/Source/ModulusCore/Private/CoreData/Tags/MCore_MenuTabTags.cpp
@@ -0,0 +1,13 @@
+﻿// Copyright 2025, Midnight Pixel Studio LLC. All Rights Reserved.
+
+#include "CoreData/Tags/MCore_MenuTabTags.h"
+
+namespace MCore_MenuTabTags
+{
+	// ========================================================================
+	// IN GAME MENU TAB TAGS
+	// ========================================================================
+
+	UE_DEFINE_GAMEPLAY_TAG(MCore_UI_MenuTab, "MCore.UI.MenuTab");
+	UE_DEFINE_GAMEPLAY_TAG(MCore_UI_MenuTab_Settings, "MCore.UI.MenuTab.Settings");
+}
---
+++ Plugins/ModulusCore/Source/ModulusCore/Public/CoreData/Tags/MCore_MenuTabTags.h
@@ -0,0 +1,22 @@
+// Copyright 2025, Midnight Pixel Studio LLC. All Rights Reserved
+
+/**
+ * MCore_SettingsTags.h
+ *
+ * Native gameplay tag declarations for the settings system.
+ * Covers categories, sub-categories, individual settings, and events.
+ */
+
+#pragma once
+
+#include "NativeGameplayTags.h"
+
+namespace MCore_MenuTabTags
+{
+	// ========================================================================
+	// IN GAME MENU TAB TAGS
+	// ========================================================================
+
+    MODULUSCORE_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(MCore_UI_MenuTab);
+    MODULUSCORE_API UE_DECLARE_GAMEPLAY_TAG_EXTERN(MCore_UI_MenuTab_Settings);
+}
